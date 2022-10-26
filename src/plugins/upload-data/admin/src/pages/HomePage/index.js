87dff85ab091a1133570730762070253dc4660b0
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2022-10-26 19:19:18 -0400

    setup api call

--- src/plugins/upload-data/admin/src/pages/HomePage/index.js
+++ src/plugins/upload-data/admin/src/pages/HomePage/index.js
@@ -4,13 +4,50 @@
  *
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, ContentLayout, BaseHeaderLayout } from '@strapi/design-system/Layout';
 import { Button } from '@strapi/design-system/Button';
+import Upload from '@strapi/icons/Upload';
 import { Select, Option } from '@strapi/design-system/Select';
 import File from '@strapi/icons/File';
 
 const HomePage = () => {
+    const [loadingMediaFiles, setLoadingMediaFiles] = useState(false);
+    const [mediaFiles, setMediaFiles] = useState(null);
+
+    /**
+     * Load media files for select
+     */
+    
+    useEffect(() => {
+        if (!mediaFiles) {
+            loadMediaFiles();
+        }
+    }, []);
+
+    const loadMediaFiles = () => {
+        setLoadingMediaFiles(true);
+
+        fetch('http://localhost:1337/api/upload/files', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.STRAPI_ADMIN_BEARER_TOKEN
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data & data.length) {
+                setMediaFiles(data);
+            }
+
+            setLoadingMediaFiles(false);
+        })
+        .catch(error => {
+            console.log('Error retrieving data: ', error);
+            setLoadingMediaFiles(false);
+        })
+    }
+
     return (
         <Layout>
             <BaseHeaderLayout
@@ -19,12 +56,17 @@ const HomePage = () => {
             />
 
             <ContentLayout>
+                {/* Select file */}
                 <Select
                     label="Choose file"
                     placeholder="Nothing selected"
                 >
                     <Option>Test</Option>
                 </Select>
+
+            {/* Update data button */}
+            <Button>Update plot data</Button>
+
             </ContentLayout>
         </Layout>
     );
