33e6a8c1df05a3c7370770004e91a57b60c16d81
Caleb Rowe <37968598+cajrowe94@users.noreply.github.com>
2022-10-29 22:17:31 -0400

    save

--- src/plugins/upload-data/server/controllers/plot-data.js
+++ src/plugins/upload-data/server/controllers/plot-data.js
@@ -12,9 +12,37 @@ const parseFile = (fileObject) => {
 		fileObject &&
 		fileObject.url
 	) {
-		const workSheetsFromFile = xlsx.parse(`/opt/app/public/${fileObject.url}`);
-		return workSheetsFromFile;
+		const parsedData = xlsx.parse(`/opt/app/public/${fileObject.url}`);
+
+		if (
+			parsedData &&
+			parsedData.length
+		) {
+			return parsedData[0].data;
+		}
+
+		return { data: [] }
 	}
+}
+
+const updatePlotData = async (parsedPlotData) => {
+	const countyName = 0,
+        plotNumber = 1,
+        plotType = 2,
+        plotProtocol = 3,
+        plotSurveyDate = 4,
+        plotCrewOne = 5,
+        plotCrewTwo = 6,
+        plotCrewThree = 7,
+        plotCrewFour = 8;
+
+    let response = {};
+
+    if (parsedPlotData.length) {
+    	parsedPlotData.forEach(item => {
+    		
+    	});
+    }
 }
 
 module.exports = {
@@ -27,10 +55,16 @@ module.exports = {
 				requestBody &&
 				requestBody.fileId
 			) {
+				// get the uploaded file
 				const file = await getFileById(requestBody.fileId);
+
+				// parse out the raw data
 				const parsedFile = parseFile(file);
 
-				ctx.body = { 'message': parsedFile }
+				// update the rows with the parsed data
+				const updateResponse = await updatePlotData(parsedFile);
+
+				ctx.body = parsedFile;
 			} else {
 				ctx.body = {
 					'message': 'fileId required'
